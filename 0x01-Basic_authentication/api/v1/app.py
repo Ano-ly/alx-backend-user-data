478257a8482f0dcf07e60a4d8185f09db7f1faa4
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-07-09 19:20:47 +0100

    Task 5

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,6 +12,25 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+if os.getenv('AUTH_TYPE') == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+@app.before_request
+def auth_b_r():
+    if auth is not None:
+        blacklist = ['/api/v1/status/',
+                     '/api/v1/unauthorized/',
+                     '/api/v1/forbidden/']
+        req_auth = auth.require_auth(request.path, blacklist)
+        if req_auth is not False:
+            if auth.authorization_header(request) is None:
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
 
 
 @app.errorhandler(404)
---
+++ 0x01-Basic_authentication/test.py
@@ -0,0 +1,5 @@
+#Pycodestyle tests
+
+pycodestyle api/v1/app.py
+pycodestyle api/v1/views/index.py
+pycodestyle api/v1/auth/auth.py
