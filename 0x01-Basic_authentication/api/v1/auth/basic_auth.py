b749263fb1f3a87d863b59ed5470a9dd5de92713
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-07-09 19:33:57 +0100

    Task 6

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Authorization class for API"""
+from flask import request
+from typing import List, TypeVar
+from api.vi.auth.auth import Auth
+
+class BasicAuth(Auth):
+    pass
