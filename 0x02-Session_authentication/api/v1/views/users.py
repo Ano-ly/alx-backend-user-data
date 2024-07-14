ffa1b4e4caf5ca164b562e26183d13bfc36485f1
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-07-14 19:07:58 +0100

    Add documentation

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -40,6 +40,7 @@ def view_one_user(user_id: str = None) -> str:
 
 @app_views.route('/users/me', methods=['GET'], strict_slashes=False)
 def get_me() -> User:
+    """Get current Authenticated User Object"""
     return (jsonify(request.current_user.to_json()))
 
 
