0d3bc804d6eb7b9a0b814b3301b0049b8e190662
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-07-14 19:03:31 +0100

    Task 1

---
+++ 0x02-Session_authentication/test.py
@@ -0,0 +1,7 @@
+#Pycodestyle tests
+
+pycodestyle api/v1/app.py
+pycodestyle api/v1/views/index.py
+pycodestyle api/v1/views/users.py
+pycodestyle api/v1/auth/auth.py
+pycodestyle api/v1/auth/basic_auth.py
