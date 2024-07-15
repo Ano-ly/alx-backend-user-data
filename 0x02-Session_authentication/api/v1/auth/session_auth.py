7e7cece11336686199cac25cf32f4cc301708f65
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-07-15 14:51:05 +0100

    Task 3

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -19,6 +19,15 @@ class SessionAuth(Auth):
             return (None)
         if type(user_id) != str:
             return (None)
-        new_id = uuid.uuid4()
+        new_id = str(uuid.uuid4())
         self.user_id_by_session_id.update({new_id: user_id})
         return (new_id)
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Obtaint the user_id attached to a session id"""
+        if session_id is None:
+            return (None)
+        if type(session_id) != str:
+            return (None)
+        return (self.user_id_by_session_id.get(session_id))
+
